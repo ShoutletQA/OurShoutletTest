7330b7d2fdac1f82118738788d528f7d4052cb18
Gene Cook <gene.cook@shoutlet.com>
2011-08-03 21:43:29 -0500

    initial put of my test cpp file

---
+++ two_functions.cpp
@@ -0,0 +1,16 @@
+// This is a test file.
+
+void main()
+{
+    int i = 0;
+    
+}
+
+/*
+ function is the first function I'm going to write.
+ But in the next branch, I want to change this function
+ */
+int primary_function( int x )
+{
+    return x++;
+}
